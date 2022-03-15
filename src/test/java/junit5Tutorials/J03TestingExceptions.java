6d2f4c04f29969cb7c10802b2822c29e81523c2a
Nuri Korganci <nkorganci@gmail.com>
2022-03-15 17:32:36 -0500

    cvs

--- src/test/java/junit5Tutorials/J03TestingExceptions.java
+++ src/test/java/junit5Tutorials/J03TestingExceptions.java
@@ -1,5 +1,9 @@
 package junit5Tutorials;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class J03TestingExceptions {
     /*
     assertThrow() metodu belirtilen islemin(executable) belirtilen turde bir Exception olusturup olusturmadigini test eder.
@@ -10,5 +14,43 @@ public class J03TestingExceptions {
    Eger islem belirtilen bir turde hata verirse test basarili olur.
    Exception turunde parent-child iliskisi var ise yine test basarili sayilir.
      */
+
+    @Test
+    void testException(){
+        String str = null;
+        // str.length(); // should return NullPointerException
+
+        assertThrows(NullPointerException.class,()->str.length());
+    }
+
+    @Test
+    void textException2(){
+        //String textNum="123";
+        //Integer num= Integer.parseInt(textNum);
+
+        assertThrows(NumberFormatException.class,()->Integer.parseInt("12a"));
+    }
+    @Test
+    void textException3(){
+        assertThrows(IllegalArgumentException.class,()->yasYazdir(-2));
+    }
+
+    void yasYazdir(int yas){
+        if(yas<0){
+            throw new IllegalArgumentException();
+        }else {
+            System.out.println(yas);
+        }
+    }
+
+
+    @Test
+    void testException4(){
+        assertThrows(ArithmeticException.class,()->bol(2,0));
+    }
+
+    int bol(int a, int b){
+        return a/b;
+    }
 
 }
---
+++ src/test/java/junit5Tutorials/J04ParameterizedTest.java
@@ -0,0 +1,41 @@
+package junit5Tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class J04ParameterizedTest {
+
+    /*
+    length() methodunun asagidaki kullar icin 0' dan buyuk deger dondurup dondurmedigini kontrol et.
+    a, ab, abb,....
+    NOT SUGGESTED
+     */
+
+    // 1 Bad practice, not recommended
+    @Test
+    void manualTesting1(){
+        assertEquals(2,"sdf".length());
+        assertEquals(2,"sdf".length());
+    }
+
+    // 2 Good practice
+    @ParameterizedTest
+    @ValueSource(strings = {"adf","adfd",""})
+    void automaticTesting(String str){
+        assertTrue(str.length()>0,"Must be bigger than 0");
+    }
+
+    @ParameterizedTest(name = "Expected:{0},Given:{1}")
+    @CsvSource(value = {"AD,ad","CD,cd"})
+    void toUpperCaseT(String expected,String str){
+        assertEquals(expected,str.toUpperCase());
+    }
+
+
+
+}
