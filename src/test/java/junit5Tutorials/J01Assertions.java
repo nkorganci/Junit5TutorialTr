f2b7109e8714ddd148228aa9d0f404652cb47f9e
Nuri Korganci <nkorganci@gmail.com>
2022-03-15 12:28:16 -0500

    junit

--- src/test/java/junit5Tutorials/J01Assertions.java
+++ src/test/java/junit5Tutorials/J01Assertions.java
@@ -2,6 +2,8 @@ package junit5Tutorials;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class J01Assertions {
 
     @Test
@@ -9,5 +11,20 @@ public class J01Assertions {
         int actual = "hello".length();
         int expected = 5;
 
+        // 1.assertEquals()
+//        assertEquals(expected,actual,"Beklenen degere esit degil, hata verirse cikar bu mesaj");
+//        assertEquals(4,actual,"Hata");
+//        assertEquals(2,"hello".length(), "yanlissa hata veriri");
+
+        // 2.assertNotEquals()
+        //assertNotEquals(expected,actual,"esitmis :)");
+
+        // 3.assertTrue()
+        expected=4;
+        assertTrue(expected==actual,"False: Esit olmaliydi");
+
+        expected=5;
+        //4. assertFalse()
+        assertFalse(expected==actual,"TRUE: Esit olmamaliydi");
     }
 }
