3daf898873755a0624d69b16f1e07465071ce975
Nuri Korganci <nkorganci@gmail.com>
2022-03-15 14:17:09 -0500

    assertions

--- src/test/java/junit5Tutorials/J01Assertions.java
+++ src/test/java/junit5Tutorials/J01Assertions.java
@@ -1,5 +1,6 @@
 package junit5Tutorials;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +27,34 @@ public class J01Assertions {
         expected=5;
         //4. assertFalse()
         assertFalse(expected==actual,"TRUE: Esit olmamaliydi");
+    }
+
+    @Test
+    void testToConvertUpper(){
+        String actual = "abcd".toUpperCase();
+        String expected = "ABCD";
+
+        assertNull(actual,"Parameter is not NULL, It was supposed to be NULL but not null");
+        assertNotNull(actual,"Not null, it has something");
+    }
+
+    @Test
+    @DisplayName("Boolean comparison")
+    void testToContain(){
+        boolean actual = "abcd".contains("hi");
+        boolean expected = true;
+         assertEquals(expected,actual,"Malesef ikiside TRUE veya FALSE degilmis");
+
+    }
+
+    @Test
+    void testWithArrays(){
+
+        String text = "Hello Enver";
+        String []arr = text.split(" ");
+        String expected[] = {"Hello","Enver"};
+
+        assertArrayEquals(expected, arr, "Arrays are not same");
+
     }
 }
